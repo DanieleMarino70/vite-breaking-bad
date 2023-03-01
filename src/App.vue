022f388eac66034c987380e298d9c4588833569b
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-01 15:46:50 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: {AppHeader}
+
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+    data(){
+        return{
+            
+        }
+    }
+
+}
+
+
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Yu-Gi-Oh Api</span>
+      </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
